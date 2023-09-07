7910672f4c2b7af02c964fc11f96e24bbce5efb6
Stephen <gstephenc@hotmail.com>
2023-09-07 13:11:10 -0400

    added js

---
+++ vignere.js
@@ -0,0 +1,12 @@
+//document.getElementById('calculate').addEventListener('click', Encrypt);
+
+function Encrypt(){
+    const text = document.getElementById('inputText').value;
+    const key = document.getElementById('keyInput').value;
+    console.log("text: " + text);
+    console.log("key: " + key);
+}
+
+function Decrypt(){
+
+}
