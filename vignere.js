6286cd96774346d1689a8e5d1ffcbcb403f833ba
Stephen <gstephenc@hotmail.com>
2023-09-10 15:31:02 -0400

    more js
    - decide what to do with punctuation and spacing
    - decide if translate all to upper or lower
    - display result in output

--- vignere.js
+++ vignere.js
@@ -1,8 +1,10 @@
 //document.getElementById('calculate').addEventListener('click', Encrypt);
 
+
 function Encrypt(){
     const text = document.getElementById('inputText').value;
     let key = document.getElementById('keyInput').value;
+    let cipher = "";
     console.log("text: " + text);
     console.log("key: " + key);
     if (key.length < text.length){
@@ -13,9 +15,15 @@ function Encrypt(){
         }
         console.log(key);
     }
-    for(const letter of text){
-        console.log(letter);
+    
+    for(let i=0; i<text.length; i++){
+        const number = ((text.charCodeAt(i)-97) + (key.charCodeAt(i)-97)) % 26;
+        const e = String.fromCharCode(number + 97);
+        console.log(e);
+        cipher += e;
     }
+
+    console.log(cipher);
 }
 
 function Decrypt(){
