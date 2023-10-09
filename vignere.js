fe55e482a8df13a9b051997e4564187e8ce02b4c
Stephen <gstephenc@hotmail.com>
2023-10-09 11:27:23 -0400

    finished error messages

--- vignere.js
+++ vignere.js
@@ -2,24 +2,39 @@
 
 
 function Vigenere(type){
+    let textErrorBool = false;
+    let keyErrorBool = false;
+    
     const text = document.getElementById('inputText').value;
     const bestText = text.toUpperCase();
     if(text.length == 0){
-        document.getElementById('textError').style.display = 'flex';
-        return;
+        textErrorBool = true;
     }
     else if (text.length != 0 && document.getElementById('textError').style.display == 'flex'){
         document.getElementById('textError').style.display = 'none';
     }
+    
     let key = document.getElementById('keyInput').value;
     key = key.toUpperCase();
     if(key.length == 0){
-        document.getElementById('keyError').style.display = 'flex';
-        return;
+        keyErrorBool = true;
     }
     else if (key.length != 0 && document.getElementById('keyError').style.display == 'flex'){
         document.getElementById('keyError').style.display = 'none';
     }
+    
+    if(textErrorBool){
+        document.getElementById('textError').style.display = 'flex';
+        if(keyErrorBool){
+            document.getElementById('keyError').style.display = 'flex';
+        }
+        return;
+    }
+    else if(keyErrorBool){
+        document.getElementById('keyError').style.display = 'flex';
+        return;
+    }
+
     let result = "";
     console.log("text: " + bestText);
     console.log("key: " + key);
